81f3ac043ea953be566b384c4f3492298d201d42
Evan Solomon <evan@evanalyze.com>
2014-03-04 15:01:05 -0800

    Add tests for async streams

--- test/filter.js
+++ test/filter.js
@@ -24,9 +24,45 @@ describe('Filter', function () {
     stream.end('Out of Cape Cod tonight')
   })
 
+  it('Should allow objects that pass async', function (done) {
+    var collector = new Collector()
+    var stream = filter(function (chunk, cb) {
+      setImmediate(function () {
+        cb(null, noJersey(chunk))
+      })
+    }).async()
+
+    stream.pipe(collector)
+    stream.on('error', done)
+    stream.on('end', function () {
+      assert.equal('Out of Cape Cod tonight', collector.getObjects().pop())
+      done()
+    })
+
+    stream.end('Out of Cape Cod tonight')
+  })
+
   it('Should block objects that do not pass', function (done) {
     var collector = new Collector()
     var stream = filter(noJersey)
+
+    stream.pipe(collector)
+    stream.on('error', done)
+    stream.on('end', function () {
+      assert.deepEqual([], collector.getObjects())
+      done()
+    })
+
+    stream.end('All the way to the Garden State')
+  })
+
+  it('Should block objects that do not pass async', function (done) {
+    var collector = new Collector()
+    var stream = filter(function (chunk, cb) {
+      setImmediate(function () {
+        cb(null, noJersey(chunk))
+      })
+    }).async()
 
     stream.pipe(collector)
     stream.on('error', done)
--- test/map.js
+++ test/map.js
@@ -25,4 +25,27 @@ describe('Map', function () {
     stream.write('Why would you lie about anything at all?')
     stream.end()
   })
+  
+  it('Should apply an async mapper', function (done) {
+    var collector = new Collector()
+    var stream = map(function (line, cb) {
+      setImmediate(function () {
+        cb(null, line.toUpperCase())
+      })
+    }).async()
+
+    stream.pipe(collector)
+    stream.on('error', done)
+    stream.on('end', function () {
+      assert.deepEqual([
+        'WHY WOULD YOU LIE ABOUT HOW MUCH COAL YOU HAVE?',
+        'WHY WOULD YOU LIE ABOUT ANYTHING AT ALL?'
+      ], collector.getObjects())
+      done()
+    })
+
+    stream.write('Why would you lie about how much coal you have?')
+    stream.write('Why would you lie about anything at all?')
+    stream.end()
+  })
 })
