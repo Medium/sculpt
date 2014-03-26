140cb407d7e0a255b2f2a88b65d46f9285f99d43
Evan Solomon <evan@evanalyze.com>
2014-03-26 16:51:37 -0700

    Everything is a Mapper now

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ var path = require('path')
 
 var libPath = path.join(__dirname, 'lib')
 fs.readdirSync(libPath).forEach(function (lib) {
-  if (lib.slice(0, 1) === '_') return
-
   var name = lib.replace('.js', '')
   exports[name] = require(path.join(libPath, name))
 })
